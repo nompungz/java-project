894586cf3eb02e83e836b4383437eb0b22e9edeb
tanapat.so <tanapat.soontornlimsiri@gmail.com>
2017-07-21 16:20:56 +0700

    midterm case 22

--- src/test/java/exam1/Exam01Test.java
+++ src/test/java/exam1/Exam01Test.java
@@ -208,7 +208,7 @@ public class Exam01Test {
         assertEquals("Thirty-Forty", actualResult);
     }
 
-    @Test @Ignore
+    @Test
     public void player_A_is_winner_with_4_2_should_Win_for_Player_A() {
         exam01.playerAWin();
         exam01.playerBWin();
