48913c6f7f5dcbea6b9132c9d80de1cb752c5fde
tanapat.so <tanapat.soontornlimsiri@gmail.com>
2017-07-21 16:15:19 +0700

    midterm case 15

--- src/test/java/exam1/Exam01Test.java
+++ src/test/java/exam1/Exam01Test.java
@@ -126,7 +126,7 @@ public class Exam01Test {
         assertEquals("Deuce", actualResult);
     }
 
-    @Test 
+    @Test
     public void normal_case_01_should_Thirty_Fifteen() {
         exam01.playerAWin();
         exam01.playerBWin();
@@ -135,7 +135,7 @@ public class Exam01Test {
         assertEquals("Thirty-Fifteen", actualResult);
     }
 
-    @Test @Ignore
+    @Test 
     public void normal_case_02_should_Fifteen_Thirty() {
         exam01.playerAWin();
         exam01.playerBWin();
